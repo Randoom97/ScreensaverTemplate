dbce53d3f972d76400641e91dbe966ce00d00552
Logan Terry <randoom97@live.com>
2017-03-14 19:33:59 -0600

    Begin work on settings form

--- ScreensaverTemplate/WindowsFormsApplication1/SettingsForm.Designer.cs
+++ ScreensaverTemplate/WindowsFormsApplication1/SettingsForm.Designer.cs
@@ -28,11 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.99234F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.00766F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 217);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Size = new System.Drawing.Size(278, 41);
+            this.splitContainer1.SplitterDistance = 135;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 41);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "SettingsForm";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ScreensaverTemplate/WindowsFormsApplication1/SettingsForm.cs
+++ ScreensaverTemplate/WindowsFormsApplication1/SettingsForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,79 @@ namespace WindowsFormsApplication1
 {
     public partial class SettingsForm : Form
     {
+        private static RegistryKey registryKey;
+        private static Dictionary<string, Control> control = new Dictionary<string, Control>();
+        private static Dictionary<string, double> doubleMin = new Dictionary<string, double>();
+        private static Dictionary<string, double> doubleMax = new Dictionary<string, double>();
+        private static Dictionary<string, int> intMin = new Dictionary<string, int>();
+        private static Dictionary<string, int> intMax = new Dictionary<string, int>();
+
         public SettingsForm()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Sets the registry subfolder to save the values in.
+        /// </summary>
+        /// <param name="key"></param>
+        public static void SetRegistryKey(string key)
+        {
+            registryKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\" + key);
+        }
+
+        /// <summary>
+        /// Adds a double field to the settings form.
+        /// This will be saved in registry for use when the program starts.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static void AddDoubleField(string name, double min, double max)
+        {
+            control.Add(name, new TextBox());
+            doubleMin.Add(name, min);
+            doubleMin.Add(name, max);
+        }
+
+        /// <summary>
+        /// Adds a integer field to the settings form.
+        /// This will be saved in registry for use when the program starts.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static void AddIntegerField(string name, int min, int max)
+        {
+            control.Add(name, new TextBox());
+            intMin.Add(name, min);
+            intMin.Add(name, max);
+        }
+
+        /// <summary>
+        /// Adds a string field to the settings form.
+        /// This will be saved in registry for use when the program starts.
+        /// </summary>
+        /// <param name="name"></param>
+        public static void AddStringField(string name)
+        {
+            control.Add(name, new TextBox());
+        }
+
+        public static string GetStringField(string name)
+        {
+            return control[name].Text;
+        }
+
+        public static double GetDoubleField(string name)
+        {
+            return double.Parse(control[name].Text);
+        }
+
+        public static int GetIntegerField(string name)
+        {
+            return int.Parse(control[name].Text);
+        }
+
     }
 }
